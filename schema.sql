20c67f169e3343c71423ab6bb84c65a0b5a713e9
Krystyn Smith <krskrsmith@gmail.com>
2021-10-17 14:56:46 -0500

    made changes to schema, started index.js

--- schema.sql
+++ schema.sql
@@ -8,24 +8,29 @@ CREATE TABLE courses (
 
 DROP TABLE IF EXISTS department;
 CREATE TABLE department (
-    id: INT PRIMARY KEY
-    name: VARSHAR(30)
-)
+    id: INT AUTO_INCREMEMT PRIMARY KEY,
+    name: VARSHAR(30) NOT NULL,
+);
 
 DROP TABLE IF EXISTS role;
 CREATE TABLE role (
-    id: INT PRIMARY KEY
-    title: VARCHAR(30)
-    salary: DECIMAL
-    department_id: INT
-)
+    id: INT AUTO_INCREMEMT PRIMARY KEY,
+    title: VARCHAR(30) NOT NULL,
+    salary: DECIMAL,
+    department_id: INT NOT NULL,
+    INDEX dept_ind (department_id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE CASCADE,
+);
 
 DROP TABLE IF EXISTS employee;
 CREATE TABLE employee (
-    id: INT PRIMARY KEY
-    first_name: VARCHAR(30)
-    last_name: VARCHAR(30)
-    role_id: INT
-    manager_id: INT
+    id: INT NOT NULL AUTO_INCREMEMT PRIMARY KEY,
+    first_name: VARCHAR(30) NOT NULL,
+    last_name: VARCHAR(30) NOT NULL,
+    role_id: INT NOT NULL,
+    INDEX role_ind (role_id),
+    manager_id: INT,
+    INDEX man_ind (manager_id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee (id) ON DELETE SET NULL,
     /*if no entry in manager_id, NULL should show
-)
+);
